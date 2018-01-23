7414b9e6f8c13d2dc2f72a2d25b5f1b86cd381b6
jeena <jeenaj@qburst.com>
2018-01-23 16:11:54 +0530

    validating form

--- scripts.js
+++ scripts.js
@@ -1,33 +1,97 @@
-console.log('hahah');
-function submitFunction()
+
+
+var letters=/^[A-Za-z, ]+$/;
+
+
+var mailvailid=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+  var re = /((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*/;
+
+  var namef=0,emailf=0,webf=0;
+function checkName(nm){
+  if((nm==null)||(!nm.match(letters))||(nm.length>25))
+  {
+    document.getElementById("errorname").innerHTML="invalid name";
+  }
+  else {
+    namef=1;
+    document.getElementById("errorname").innerHTML=" ";
+  }
+}
+function checkEmail(mail)
+{
+  var atpos=mail.indexOf("@");
+var dotpos=mail.lastIndexOf(".");
+  if(atpos==null || atpos<1 || dotpos<atpos+2|| dotpos+2>=mail.length || dotpos==null ||(!mail.match(mailvailid)))
+  {
+
+    document.getElementById("erroremail").innerHTML="invalid email";
+  }
+  else {
+    emailf=1;
+    document.getElementById("erroremail").innerHTML=" ";
+  }
+}
+function checkweb(url)
+{
+  if (!url.match(re))
     {
-      var nm=document.myform.name.value;
-      var errmsg="";
-      var letters=/^[A-Za-z, ]+$/;
-      var mail=document.myform.email.value;
-      var atpos=mail.indexOf("@");
-      var mailvailid=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-      var dotpos=mail.lastIndexOf(".");
-      var re = /((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*/;
-      var web=document.myform.url.value;
-      if((nm==null)||(!nm.match(letters))||(nm.length>25))
-      {
-        errmsg=" give a valid name ";
-      }
-      if(atpos==null || atpos<1 || dotpos<atpos+2|| dotpos+2>=mail.length || dotpos==null ||(!mail.match(mailvailid)))
-      {
-        errmsg=errmsg+"give a valid email ";
 
-      }
-
-     if (!web.match(re))
-       {
-         errmsg=errmsg+"give a valid url";
-
-      }
-      if(errmsg!="")
-      {
-      alert(errmsg);
+      document.getElementById("errorweb").innerHTML="invalid url";
     }
-      return false;
+    else {
+
+      document.getElementById("errorweb").innerHTML=" ";
     }
+
+}
+function checkfield()
+{
+  var name=document.getElementById("name").value;
+  var email=document.getElementById("email").value;
+
+  if(name.length==0)
+  {
+    document.getElementById("errorname").innerHTML="name field is empty";
+  }
+  else {
+
+    namef=2;
+      checkName(name);
+    document.getElementById("errorname").innerHTML=" ";
+  }
+
+  if(email.length==0)
+  {
+    document.getElementById("erroremail").innerHTML="email field is empty";
+  }
+  else {
+    emailf=2;
+    checkEmail(email);
+
+    document.getElementById("erroremail").innerHTML=" ";
+  }
+
+if(namef==1){
+  if(emailf==1){
+   {
+
+  alert("submit successfully");
+}
+}
+}
+else if(namef==2 && emailf==2)
+{
+  alert("name  & email incorrect format");
+}
+else if(emailf==2)
+{
+  alert("email incorrect format");
+
+}
+else if(namef==2 )
+{
+  alert("name incorrect format");
+}
+
+}
